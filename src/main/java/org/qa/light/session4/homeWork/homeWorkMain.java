c7df7037117871879c80887a5500e9055976e8ae
A1exenderS <vovnjanko.sasha2109@gmail.com>
2025-10-12 20:43:48 +0300

    added checkException method

--- src/main/java/org/qa/light/session4/homeWork/homeWorkMain.java
+++ src/main/java/org/qa/light/session4/homeWork/homeWorkMain.java
@@ -7,15 +7,24 @@ public class homeWorkMain {
         Phone xiaomi = new Android("Samsung Galaxy S25 Ulta", "black");
 
         Phone iPhone = new IOS("IPhone 17 Pro Max", "black");
-        Phone iPhone2 = new IOS(null, "black");
+        Phone iPhone2 = new IOS("IPhone 17 Pro Max", null);
 
-        System.out.println(xiaomi.modelName.equals(iPhone2.modelName));
+        System.out.println(xiaomi.modelName.equals(iPhone.modelName));
         System.out.println(xiaomi.color.equals(iPhone2.color));
         System.out.println(xiaomi.hashCode());
         System.out.println(iPhone.hashCode());
-        getHashWithNullModelName(iPhone2);
-        getHashWithNullModelName(iPhone);
-        getHashWithNullModelName(xiaomi);
+        System.out.println("----------------------");
+        System.out.println("----------------------");
+        System.out.println("----------------------");
+        checkException(xiaomi, iPhone);
+        System.out.println("----------------------");
+        checkException(iPhone, iPhone2);
+        System.out.println("----------------------");
+        checkException(iPhone2, iPhone);
+
+//        getHashWithNullModelName(iPhone2);
+//        getHashWithNullModelName(iPhone);
+//        getHashWithNullModelName(xiaomi);
     }
 
     public static void getHashWithNullModelName(Phone phone) {
@@ -26,6 +35,21 @@ public class homeWorkMain {
 
             // works only if model and color == null, if one of them exists - hash also exists
             System.out.println("The phone model is Null - fix it!");
+        }
+    }
+
+    public static void checkException(Phone phone, Phone phone1) {
+        try {
+            System.out.println("Say, the model is the same?    ..    " + phone.modelName.equals(phone1.modelName));
+
+        } catch (Exception e) {
+            System.out.println("One of the phones has NULL model name");
+        }
+
+        try {
+            System.out.println("Say, the color is the same?    ..    " + phone.color.equals(phone1.color));
+        } catch (Exception e) {
+            System.out.println("One of the phones has NULL color");
         }
     }
 }
